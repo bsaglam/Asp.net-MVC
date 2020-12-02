917d94f0c23392c3ed77182f2c2c87cfdafbfe57
bsaglam <burcu.saglam001@gmail.com>
2020-12-02 12:37:07 +0300

    Kullanıcı ekleme ekranı eklendi.

---
+++ section-7-Entity/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using section_7_Entity.Context;
+using section_7_Entity.Models;
+using section_7_Entity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace section_7_Entity.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddUser()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserViewModel model)
+        {
+            DatabaseContext db = new DatabaseContext();
+            Users user = new Users()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Address = model.Address
+            };
+            db.UsersEntity.Add(user);
+            int result = db.SaveChanges();
+            ViewBag.Result= (result > 0) ? "success" :  ViewBag.result = "danger";
+
+            return View();
+        }
+    }
+}
---
+++ section-7-Entity/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using section_7_Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace section_7_Entity.ViewModels
+{
+    public class UserViewModel
+    {
+        
+        public string Name { get; set; }
+        
+        public string Surname { get; set; }
+        public  List<Addresses> Address { get; set; }
+    }
+}

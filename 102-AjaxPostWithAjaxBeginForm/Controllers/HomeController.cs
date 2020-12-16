6328feb494d58ddb12d2696cb29f66c654fcee1d
bsaglam <burcu.saglam001@gmail.com>
2020-12-16 03:27:37 +0300

    ajaxPost-with beginForm

---
+++ 102-AjaxPostWithAjaxBeginForm/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using _102_AjaxPostWithAjaxBeginForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _102_AjaxPostWithAjaxBeginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new ToDoItem());
+        }
+
+        public PartialViewResult AddData(ToDoItem model)
+        {
+            return PartialView("_ToDoListPartial", model);
+        }
+    }
+}
